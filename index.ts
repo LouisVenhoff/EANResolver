6240567cfea24681943ec5f11a9ea1f4d81a217a
Louis Venhoff <louisvenhoff@Mini-von-Louis.fritz.box>
2024-03-31 15:36:30 +0200

    Added npm package information

--- index.ts
+++ index.ts
@@ -4,16 +4,20 @@ import { MinimalProduct } from "./types/eanResolverTypes";
 
 const api:ApiController = new ApiController("87.106.127.217", 3014);
 
-export const checkEan = (ean:string) =>{
+const checkEan = (ean:string) =>{
     return EanHelper.validateEAN(ean);
 }
 
-export const getProduct = async (ean:string):Promise<MinimalProduct> => {
+const getProduct = async (ean:string):Promise<MinimalProduct> => {
     let product:MinimalProduct = await api.resolveEan(ean);
 
     return new Promise((resolve, reject) => {resolve(product)});
     
 }
+
+
+module.exports = checkEan;
+module.exports = getProduct;
 
 
 
