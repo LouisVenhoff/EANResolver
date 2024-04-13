66f2d78c35e2f81fe684d47ca7d6f43bba41d6b3
Louis Venhoff <louisvenhoff@Mini-von-Louis.fritz.box>
2024-04-13 18:15:08 +0200

    Fixed project structure

--- index.ts
+++ index.ts
@@ -2,22 +2,33 @@ import ApiController from "./libs/apiController/apiController";
 import EanHelper from "./libs/helpers/eanHelper";
 import { MinimalProduct } from "./types/eanResolverTypes";
 
-const api:ApiController = new ApiController("87.106.127.217", 3014);
 
-const checkEan = (ean:string) =>{
-    return EanHelper.validateEAN(ean);
-}
+class Resolver {
+    private api:ApiController = new ApiController("87.106.127.217", 3014);
 
-const getProduct = async (ean:string):Promise<MinimalProduct> => {
-    let product:MinimalProduct = await api.resolveEan(ean);
-
-    return new Promise((resolve, reject) => {resolve(product)});
+    public checkEan(ean:string){
+        return EanHelper.validateEAN(ean);
+    }
     
+    public async getProduct(ean:string):Promise<MinimalProduct>{
+        
+        let eanValid:boolean = this.checkEan(ean);
+
+        if(!eanValid){
+            return new Promise((resolve, reject) => {resolve({error: 2, name: "", ean: ean })});
+        }
+        
+        
+        let product:MinimalProduct = await this.api.resolveEan(ean);
+    
+        return new Promise((resolve, reject) => {resolve(product)});
+ 
+    }
 }
 
+module.exports = Resolver;
 
-module.exports = checkEan;
-module.exports = getProduct;
+export default Resolver;
 
 
 
